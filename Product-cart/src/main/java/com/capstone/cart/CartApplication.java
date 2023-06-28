c058b4eca3cd04f1d8975b26ddc4535466ffa9b3
Aadil Abu Antrollil(UST <245268@ust.com>
2023-06-28 23:39:28 +0530

    eureka client server and api gateway added

--- Product-cart/src/main/java/com/capstone/cart/CartApplication.java
+++ Product-cart/src/main/java/com/capstone/cart/CartApplication.java
@@ -5,16 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.mail.MessagingException;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class CartApplication {
-	@Autowired
-	private CartService senderService;
+//	@Autowired
+//	private CartService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartApplication.class, args);
--- contact/src/main/java/com/UST/contact/controller/ContactController.java
+++ contact/src/main/java/com/UST/contact/controller/ContactController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/contacts")
+@RequestMapping("/api/contact")
 public class ContactController {
 
     @Autowired
